93066c0a125969a58d1d0d32de7c16e22abf360a
unknown <iramatseiko2002@gmail.com>
2020-11-30 19:53:17 +0200

    Initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC\Desktop\IRA\СШІ\2 курс\Прикладне програмування\lab4\venv
+set VIRTUAL_ENV=C:\Users\soles\Desktop\GEO\GeoClock_back\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
